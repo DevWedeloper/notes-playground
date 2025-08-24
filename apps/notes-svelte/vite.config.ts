6a549b39bcf2535655d45469bfaea9ad57c80925
DevWedeloper <vicnathangabrielle@gmail.com>
2025-08-25 07:01:51 +0800

    refactor: change port

--- apps/notes-svelte/vite.config.ts
+++ apps/notes-svelte/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/notes-svelte',
   server: {
-    port: 4200,
+    port: 5173,
     host: 'localhost',
   },
   preview: {
