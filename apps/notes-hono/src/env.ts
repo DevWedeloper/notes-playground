89d61bea9b94f548c6bcdb0b8a46b9c0cca59c81
DevWedeloper <vicnathangabrielle@gmail.com>
2025-09-08 07:23:01 +0800

    feat: create env.ts

---
+++ apps/notes-hono/src/env.ts
@@ -0,0 +1,30 @@
+import { workspaceRoot } from '@nx/devkit';
+import { config } from 'dotenv';
+import path from 'path';
+import z from 'zod';
+
+config({ path: path.resolve(workspaceRoot, '.env') });
+
+const envSchema = z.object({
+  NODE_ENV: z.string().default('development'),
+  LOG_LEVEL: z.enum([
+    'fatal',
+    'error',
+    'warn',
+    'info',
+    'debug',
+    'trace',
+    'silent',
+  ]),
+});
+
+const { data: env, error } = envSchema.safeParse(process.env);
+
+if (error) {
+  console.error('❌ Invalid env:');
+  console.error(JSON.stringify(error.flatten().fieldErrors, null, 2));
+  process.exit(1);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+export default env!;
