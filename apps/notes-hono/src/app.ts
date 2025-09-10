b882ab34f9c65219c51e3915c603eabc038437fa
DevWedeloper <vicnathangabrielle@gmail.com>
2025-09-10 13:28:45 +0800

    feat: export type for client

--- apps/notes-hono/src/app.ts
+++ apps/notes-hono/src/app.ts
@@ -5,10 +5,12 @@ import tasks from './routes/tasks';
 
 const app = createApp();
 
-const routes = [index, tasks];
+const routes = [index, tasks] as const;
 
 configureOpenAPI(app);
 
 routes.forEach((route) => app.route('/', route));
+
+export type AppType = typeof routes[number];
 
 export default app;
