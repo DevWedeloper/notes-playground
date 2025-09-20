35b43f8cd9ff00df59895e7e0cf57abce5ef15f7
DevWedeloper <vicnathangabrielle@gmail.com>
2025-09-20 19:00:10 +0800

    feat: create changes for vercel serverless deployment

--- libs/core/src/db/index.ts
+++ libs/core/src/db/index.ts
@@ -1,10 +1,24 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import env from '../../env';
+import nodeFetch from "node-fetch";
 
 const db = drizzle({
   connection: {
     url: env.DATABASE_URL,
     authToken: env.DATABASE_AUTH_TOKEN,
+    fetch: async (request: Request) => {
+      const decoder = new TextDecoder();
+      let body = "{}";
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      for await (const chunk of request.body!) {
+        body = decoder.decode(chunk);
+      }
+      return nodeFetch(request.url, {
+        method: "post",
+        headers: Object.fromEntries([...request.headers.entries()]),
+        body,
+      });
+    },
   },
   casing: 'snake_case',
 });
