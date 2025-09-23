7edb1c2593d86ad28c97ae15080d76f26dbb0097
DevWedeloper <vicnathangabrielle@gmail.com>
2025-09-23 15:57:19 +0800

    fix: attempt to fix deployment issue

--- apps/notes-hono/api/index.ts
+++ apps/notes-hono/api/index.ts
@@ -1,5 +1,7 @@
 import { handle } from "@hono/node-server/vercel";
 
-import app from "../dist/apps/notes-hono/src/app.js";
+import app from "../dist/index.js";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 export default handle(app);
